9901bbfce8635b35f88831403c8ab4605662573c
Gyrefallon <gonzalo.2800@gmail.com>
2023-02-15 11:33:00 -0300

    Cambio de variables a ingles(solo JS)

--- js/main.js
+++ js/main.js
@@ -1,50 +1,49 @@
 
-    document.getElementById("numeros").addEventListener("keydown", e => e.keyCode != 38 && e.keyCode != 40 && e.preventDefault()); //El usuario solo puede utlizar las flechas arriba y abajo en el teclado
-    const input = document.querySelector("#numeros"); //Selecciona la etiqueta que tenga un id, aqui por ejemplo está seleccionando a la etiqueta que tenga el ID de numeros
-    const valueInput = document.getElementById("texto"); //Selecciona todos los elementos con la ID especificada.
-    input.addEventListener('change',numero);//el addEventListener recibe dos parametros toma el evento change  y lo pone a escuchar,
+    document.getElementById("number").addEventListener("keydown", e => e.keyCode != 38 && e.keyCode != 40 && e.preventDefault()); //El usuario solo puede utlizar las flechas arriba y abajo en el teclado
+    const input = document.querySelector("#number"); //Selecciona la etiqueta que tenga un id, aqui por ejemplo está seleccionando a la etiqueta que tenga el ID de numeros
+    const valueInput = document.getElementById("text"); //Selecciona todos los elementos con la ID especificada.
+    input.addEventListener('change',numbers);//el addEventListener recibe dos parametros toma el evento change  y lo pone a escuchar,
 //y un segundo argumento para llamar cada vez que se desencadena el evento descrito.
 
 
-function numero(numeros){
-    if ((numeros.target.value>0) && (numeros.target.value<=3)){ //si el valor de la etiqueta es mayor a 0 y es menor o igual a 3 entonces:
+function numbers(number){
+    if ((number.target.value>0) && (number.target.value<=3)){ //si el valor de la etiqueta es mayor a 0 y es menor o igual a 3 entonces:
         valueInput.innerText = "Muy deficiente";//Muestra eso en la pantalla
     }
-    if ((numeros.target.value>3) && (numeros.target.value<=5)){
+    if ((number.target.value>3) && (number.target.value<=5)){
         valueInput.innerText = "Insuficiente";
     }
-    if ((numeros.target.value>5) && (numeros.target.value<=6)){
+    if ((number.target.value>5) && (number.target.value<=6)){
         valueInput.innerText = "Suficiente";
     }
-    if ((numeros.target.value>6) && (numeros.target.value<=7)){
+    if ((number.target.value>6) && (number.target.value<=7)){
         valueInput.innerText = "Bien";
     }
-    if ((numeros.target.value>7) && (numeros.target.value<=9)){
+    if ((number.target.value>7) && (number.target.value<=9)){
         valueInput.innerText = "Notable";
     }
-    if ((numeros.target.value>9) && (numeros.target.value<=10)){
+    if ((number.target.value>9) && (number.target.value<=10)){
         valueInput.innerText = "Excelente";
     }}
 
-
-function nComentario(){
+function nComment(){//Función para hacer un sistema local de comentarios (Se reinician los comentarios una vez se actualiza la página)
     let li = document.createElement("li");
-    let valorIngresado = document.getElementById("nuevocomentario").value;
-    let text = document.createTextNode(valorIngresado);
+    let enteredValue = document.getElementById("newcomment").value;
+    let text = document.createTextNode(enteredValue);
     li.appendChild(text);
 
-    if(valorIngresado === ''){
+    if(enteredValue === ''){
         alert("Ingrese un comentario!")
     } else {
-        document.getElementById("comentarios").appendChild(li);
+        document.getElementById("comments").appendChild(li);
     }
-    document.getElementById("nuevocomentario").value = "";
-    li.className = "comentario";
+    document.getElementById("newcomment").value = "";
+    li.className = "comment";
 
-    let borrar = document.createElement("p");
-    borrar.innerHTML = ("Borrar");
-    borrar.className = "close";
-    li.appendChild(borrar);
+    let erase = document.createElement("p");
+    erase.innerHTML = ("Borrar");//El parrafo va a contener el texto de Borrar.
+    erase.className = "close";
+    li.appendChild(erase);
 
     let close = document.getElementsByClassName("close");
     let i;
